27b1981f0e959b38c1853c71d19dcaf6887c8f6d
rin1208 <sora1208sky@gmail.com>
2019-02-11 17:20:30 +0900

    ok

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/exec"
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	var siteurl string
+	var a string
+	fmt.Print("site URL or image URL? :")
+	fmt.Scan(&a)
+	if a == "site" {
+		defer fmt.Println("Save completed!!")
+		var result []*url.URL
+		fmt.Print("Please enter URL :")
+		fmt.Scan(&siteurl)
+		confirmation:= confirmurl(siteurl)
+		if confirmation == true {
+			doc, _ := goquery.NewDocument(siteurl)
+			doc.Find("img").Each(func(_ int, s *goquery.Selection) {
+				target, _ := s.Attr("src")
+				base, _ := url.Parse(siteurl)
+				targets, _ := url.Parse(target)
+				result = append(result, base.ResolveReference(targets))
+			})
+			for _, b := range result {
+				a := b.String()
+				log.Println(a)
+				_, err := exec.Command("wget", a).Output()
+				if err != nil {
+					log.Fatal("Save failed...", err)
+					}
+				}
+			}
+		} else if a == "image" {
+			defer fmt.Println("Save completed!!")
+			fmt.Print("Please enter URL : ")
+			fmt.Scan(&siteurl)
+			confirmation := confirmurl(siteurl)
+			if confirmation == true {
+				_, err := exec.Command("wget", siteurl).Output()
+				if err != nil {
+					log.Fatal("Save failed...", err)
+				}
+			}
+		} else if  a != "site" && a != "image" {
+			fmt.Println("Please enter either")
+		}
+}
+
+func confirmurl(url string) bool {
+	_, err := http.Get(url)
+	if err != nil {
+		fmt.Println("URL that does not exist")
+		os.Exit(0)
+	}
+	use := true
+	return use
+}
