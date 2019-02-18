d23872858dbcf17a76eb718afa0752c873b331ba
rin1208 <sora1208sky@gmail.com>
2019-02-18 14:19:00 +0900

    ok

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/url"
 	"os"
 	"os/exec"
+
 	"github.com/PuerkitoBio/goquery"
 )
 
@@ -16,42 +17,50 @@ func main() {
 	fmt.Print("site URL or image URL? :")
 	fmt.Scan(&a)
 	if a == "site" {
-		defer fmt.Println("Save completed!!")
-		var result []*url.URL
-		fmt.Print("Please enter URL :")
-		fmt.Scan(&siteurl)
-		confirmation:= confirmurl(siteurl)
-		if confirmation == true {
-			doc, _ := goquery.NewDocument(siteurl)
-			doc.Find("img").Each(func(_ int, s *goquery.Selection) {
-				target, _ := s.Attr("src")
-				base, _ := url.Parse(siteurl)
-				targets, _ := url.Parse(target)
-				result = append(result, base.ResolveReference(targets))
-			})
-			for _, b := range result {
-				a := b.String()
-				log.Println(a)
-				_, err := exec.Command("wget", a).Output()
-				if err != nil {
-					log.Fatal("Save failed...", err)
-					}
-				}
+		savesite(siteurl)
+	} else if a == "image" {
+		saveimg(siteurl)
+	} else if a != "site" && a != "image" {
+		fmt.Println("Please enter either")
+	}
+}
+
+func savesite(siteurl string) {
+	defer fmt.Println("Save completed!!")
+	var result []*url.URL
+	fmt.Print("Please enter URL :")
+	fmt.Scan(&siteurl)
+	confirmation := confirmurl(siteurl)
+	if confirmation == true {
+		doc, _ := goquery.NewDocument(siteurl)
+		doc.Find("img").Each(func(_ int, s *goquery.Selection) {
+			target, _ := s.Attr("src")
+			base, _ := url.Parse(siteurl)
+			targets, _ := url.Parse(target)
+			result = append(result, base.ResolveReference(targets))
+		})
+		for _, b := range result {
+			a := b.String()
+			log.Println(a)
+			_, err := exec.Command("wget", a).Output()
+			if err != nil {
+				log.Fatal("Save failed...", err)
 			}
-		} else if a == "image" {
-			defer fmt.Println("Save completed!!")
-			fmt.Print("Please enter URL : ")
-			fmt.Scan(&siteurl)
-			confirmation := confirmurl(siteurl)
-			if confirmation == true {
-				_, err := exec.Command("wget", siteurl).Output()
-				if err != nil {
-					log.Fatal("Save failed...", err)
-				}
-			}
-		} else if  a != "site" && a != "image" {
-			fmt.Println("Please enter either")
 		}
+	}
+}
+
+func saveimg(siteurl string) {
+	defer fmt.Println("Save completed!!")
+	fmt.Print("Please enter URL : ")
+	fmt.Scan(&siteurl)
+	confirmation := confirmurl(siteurl)
+	if confirmation == true {
+		_, err := exec.Command("wget", siteurl).Output()
+		if err != nil {
+			log.Fatal("Save failed...", err)
+		}
+	}
 }
 
 func confirmurl(url string) bool {
@@ -63,3 +72,4 @@ func confirmurl(url string) bool {
 	use := true
 	return use
 }
+
